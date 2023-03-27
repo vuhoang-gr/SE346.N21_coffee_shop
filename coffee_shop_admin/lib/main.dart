b20f1def2babdfa49a0af10a484612bc3a9fa568
vuhoang.vh <21520243@gm.uit.edu.vn>
2023-03-27 17:06:17 +0700

    dev: Create admin folder

---
+++ coffee_shop_admin/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'screens/screens.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Coffee Shop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(title: 'Coffee shop Admin page'),
+    );
+  }
+}
