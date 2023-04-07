b7502636a31fff28c342e603cfb2802ea417c800
vuhoang.vh <21520243@gm.uit.edu.vn>
2023-04-08 01:56:33 +0700

    dev: validation, add icons

---
+++ coffee_shop_app/lib/utils/validations/password_validate.dart
@@ -0,0 +1,19 @@
+import 'package:coffee_shop_app/utils/validations/validator.dart';
+
+class PasswordValidator extends Validator {
+  @override
+  bool validate(String? value) {
+    if (value == null || value.isEmpty) return false;
+    RegExp exp = RegExp(r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$');
+    if (exp.hasMatch(value)) return true;
+    return false;
+  }
+
+  @override
+  String? validator(String? value) {
+    if (!validate(value)) {
+      return "8 characters, at least 1 letter, 1 number.";
+    }
+    return null;
+  }
+}
