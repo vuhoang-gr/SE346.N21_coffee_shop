589632c20a31291c322da107989bab0a2c16ec39
kimthu09 <21521495@gm.uit.edu.vn>
2023-05-04 13:13:43 +0700

    dev: timer cubit

--- coffee_shop_app/lib/main.dart
+++ coffee_shop_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:coffee_shop_app/routing/app_router.dart';
 import 'package:coffee_shop_app/services/blocs/address_store/address_store_bloc.dart';
 import 'package:coffee_shop_app/services/blocs/cart_cubit/cart_cubit.dart';
 import 'package:coffee_shop_app/services/blocs/edit_address/edit_address_bloc.dart';
+import 'package:coffee_shop_app/services/blocs/pickup_timer/pickup_timer_cubit.dart';
 import 'package:coffee_shop_app/services/blocs/recent_see_products/recent_see_products_bloc.dart';
 import 'package:coffee_shop_app/services/blocs/search_product/search_product_bloc.dart';
 import 'package:coffee_shop_app/services/blocs/search_store/search_store_bloc.dart';
@@ -41,6 +42,9 @@ class MyApp extends StatelessWidget {
         ),
         BlocProvider<AddressStoreBloc>(
           create: (BuildContext context) => AddressStoreBloc(),
+        ),
+        BlocProvider<TimerCubit>(
+          create: (BuildContext context) => TimerCubit(),
         ),
       ],
       child: MaterialApp(
