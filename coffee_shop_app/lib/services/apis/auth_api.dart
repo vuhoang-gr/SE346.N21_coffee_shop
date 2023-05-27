5fa2a23042072c8817022a58438c2f746d6020e4
vuhoang.vh <21520243@gm.uit.edu.vn>
2023-05-27 13:41:30 +0700

    dev: AuthBloc

--- coffee_shop_app/lib/services/apis/auth_api.dart
+++ coffee_shop_app/lib/services/apis/auth_api.dart
@@ -1,4 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:coffee_shop_app/services/models/user.dart';
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthAPI {
   //singleton
@@ -8,16 +11,18 @@ class AuthAPI {
   }
   AuthAPI._internal();
 
-  final firebaseAuth = FirebaseAuth.instance;
+  final firebaseAuth = firebase_auth.FirebaseAuth.instance;
 
-  emailSignUp(String email, String password) async {
+  static User? currentUser;
+
+  Future<User?> emailSignUp(String email, String password) async {
     try {
       final credential = await firebaseAuth.createUserWithEmailAndPassword(
         email: email,
         password: password,
       );
-      return credential;
-    } on FirebaseAuthException catch (e) {
+      return toUser(credential.user);
+    } on firebase_auth.FirebaseAuthException catch (e) {
       if (e.code == 'weak-password') {
         print('The password provided is too weak.');
       } else if (e.code == 'email-already-in-use') {
@@ -26,23 +31,95 @@ class AuthAPI {
     } catch (e) {
       print(e);
     }
+    return null;
   }
 
-  emailLogin(String email, String password) async {
+  Future<User?> emailLogin(String email, String password) async {
     try {
       final credential = await firebaseAuth.signInWithEmailAndPassword(
           email: email, password: password);
-      return credential;
-    } on FirebaseAuthException catch (e) {
+      print('Login Success');
+      return toUser(credential.user);
+    } on firebase_auth.FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
         print('No user found for that email.');
       } else if (e.code == 'wrong-password') {
         print('Wrong password provided for that user.');
       }
     }
+    return null;
+  }
+
+  Future<User?> googleLogin() async {
+    if (GoogleSignIn().currentUser != null) {
+      await GoogleSignIn().disconnect();
+    }
+    try {
+      GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+      firebase_auth.AuthCredential credential =
+          firebase_auth.GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+      var rawUser = await firebaseAuth.signInWithCredential(credential);
+      return toUser(rawUser.user);
+    } on firebase_auth.FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+    return null;
+  }
+
+  Future<User?> facebookLogin() async {
+    await FacebookAuth.instance.logOut();
+    await FacebookAuth.i.logOut();
+    try {
+      final LoginResult loginResult = await FacebookAuth.instance.login();
+
+      // Create a credential from the access token
+      final firebase_auth.OAuthCredential facebookAuthCredential =
+          firebase_auth.FacebookAuthProvider.credential(
+              loginResult.accessToken!.token);
+
+      // Once signed in, return the UserCredential
+      var rawUser =
+          await firebaseAuth.signInWithCredential(facebookAuthCredential);
+      return toUser(rawUser.user);
+    } on firebase_auth.FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      } else if (e.code == 'account-exists-with-different-credential') {
+        print('account-exists-with-different-credential');
+      }
+    }
+    return null;
+  }
+
+  User? toUser(firebase_auth.User? raw) {
+    if (raw == null) return null;
+    var user = User(
+        id: raw.uid,
+        name: raw.displayName != null ? raw.displayName! : 'No Name',
+        phoneNumber:
+            raw.phoneNumber != null ? raw.phoneNumber! : 'No phone number',
+        email: raw.email!,
+        isActive: true);
+    if (raw.photoURL != null) {
+      user.avatarUrl = raw.photoURL!;
+    }
+    return user;
   }
 
   signOut() async {
     await firebaseAuth.signOut();
+    currentUser = null;
   }
 }
---
+++ coffee_shop_app/lib/services/blocs/auth_action/auth_action_state.dart
@@ -0,0 +1,23 @@
+part of 'auth_action_cubit.dart';
+
+@immutable
+abstract class AuthActionState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class Login extends AuthActionState {
+  final String? email;
+  Login({this.email});
+  @override
+  List<Object?> get props => [email];
+}
+
+class SignIn extends AuthActionState {}
+
+class ForgotPassword extends AuthActionState {
+  final String? email;
+  ForgotPassword({this.email});
+  @override
+  List<Object?> get props => [email];
+}
