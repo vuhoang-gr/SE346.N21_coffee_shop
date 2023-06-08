4f4c549dc00d8031162b41228d7cf6b82b31c9bd
bvquoc <bvquoc.dev@gmail.com>
2023-06-09 00:03:45 +0700

    fix: empty value on edit screen

--- coffee_shop_ad/lib/screens/drink_manage/size_edit.dart
+++ coffee_shop_ad/lib/screens/drink_manage/size_edit.dart
@@ -29,6 +29,14 @@ class EditSizeScreen extends StatefulWidget {
 class _EditSizeScreenState extends State<EditSizeScreen> {
   TextEditingController sizeNameController = TextEditingController();
   TextEditingController sizePriceController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    sizeNameController.text = widget.product.name;
+    sizePriceController.text = (widget.product.price.toInt()).toString();
+  }
+
   bool _isKeyboardOpened = false;
   String imageUrl = "";
   XFile? image;
@@ -50,8 +58,7 @@ class _EditSizeScreenState extends State<EditSizeScreen> {
         context: context,
         builder: (BuildContext context) {
           return AlertDialog(
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
             title: Text('Please choose media to select'),
             content: SizedBox(
               height: MediaQuery.of(context).size.height / 6,
@@ -91,8 +98,7 @@ class _EditSizeScreenState extends State<EditSizeScreen> {
   }
 
   bool _isValidData() {
-    if (sizePriceController.text.isNotEmpty &&
-        int.tryParse(sizePriceController.text) == null) return false;
+    if (sizePriceController.text.isNotEmpty && int.tryParse(sizePriceController.text) == null) return false;
     return true;
   }
 
@@ -116,17 +122,10 @@ class _EditSizeScreenState extends State<EditSizeScreen> {
 
     try {
       // update firestore
-      FirebaseFirestore.instance
-          .collection("Size")
-          .doc(widget.product.id)
-          .update({
+      FirebaseFirestore.instance.collection("Size").doc(widget.product.id).update({
         "image": image != null ? "" : widget.product.image,
-        "name": sizeNameController.text.isNotEmpty
-            ? sizeNameController.text
-            : widget.product.name,
-        "price": sizePriceController.text.isNotEmpty
-            ? int.parse(sizePriceController.text)
-            : widget.product.price,
+        "name": sizeNameController.text.isNotEmpty ? sizeNameController.text : widget.product.name,
+        "price": sizePriceController.text.isNotEmpty ? int.parse(sizePriceController.text) : widget.product.price,
       }).then((value) async {
         if (image != null) {
           // remove old image
@@ -134,15 +133,11 @@ class _EditSizeScreenState extends State<EditSizeScreen> {
 
           // upload new img
           final storageRef = FirebaseStorage.instance.ref();
-          final SizeImagesRef = storageRef
-              .child('products/Size/Size${DateTime.now().toString()}');
+          final SizeImagesRef = storageRef.child('products/Size/Size${DateTime.now().toString()}');
 
           await SizeImagesRef.putFile(File(image!.path)).then((res) {
             res.ref.getDownloadURL().then((url) {
-              FirebaseFirestore.instance
-                  .collection("Size")
-                  .doc(widget.product.id)
-                  .update({
+              FirebaseFirestore.instance.collection("Size").doc(widget.product.id).update({
                 "image": url,
               });
             });
@@ -195,10 +190,7 @@ class _EditSizeScreenState extends State<EditSizeScreen> {
                             ),
                             Container(
                               clipBehavior: Clip.hardEdge,
-                              margin: EdgeInsets.only(
-                                  left: Dimension.height16,
-                                  right: Dimension.height16,
-                                  top: 3),
+                              margin: EdgeInsets.only(left: Dimension.height16, right: Dimension.height16, top: 3),
                               decoration: BoxDecoration(
                                 color: Colors.white,
                                 borderRadius: BorderRadius.circular(8),
@@ -209,56 +201,40 @@ class _EditSizeScreenState extends State<EditSizeScreen> {
                                   SizedBox(height: 16),
                                   image != null
                                       ? Padding(
-                                          padding: const EdgeInsets.symmetric(
-                                              horizontal: 20),
+                                          padding: const EdgeInsets.symmetric(horizontal: 20),
                                           child: ClipRRect(
-                                            borderRadius:
-                                                BorderRadius.circular(8),
+                                            borderRadius: BorderRadius.circular(8),
                                             child: Image.file(
                                               File(image!.path),
                                               fit: BoxFit.cover,
-                                              width: MediaQuery.of(context)
-                                                  .size
-                                                  .width,
+                                              width: MediaQuery.of(context).size.width,
                                               height: 300,
                                             ),
                                           ),
                                         )
                                       : Padding(
-                                          padding: const EdgeInsets.symmetric(
-                                              horizontal: 20),
+                                          padding: const EdgeInsets.symmetric(horizontal: 20),
                                           child: ClipRRect(
-                                            borderRadius:
-                                                BorderRadius.circular(8),
+                                            borderRadius: BorderRadius.circular(8),
                                             child: CachedNetworkImage(
                                               alignment: Alignment.center,
                                               width: double.maxFinite,
                                               imageUrl: widget.product.image,
-                                              placeholder: (context, url) =>
-                                                  Container(
+                                              placeholder: (context, url) => Container(
                                                 alignment: Alignment.center,
-                                                child:
-                                                    const CircularProgressIndicator(),
+                                                child: const CircularProgressIndicator(),
                                               ),
-                                              errorWidget:
-                                                  (context, url, error) =>
-                                                      Container(),
+                                              errorWidget: (context, url, error) => Container(),
                                             ),
                                           ),
                                         ),
                                   ElevatedButton(
                                     style: ButtonStyle(
-                                        elevation:
-                                            const MaterialStatePropertyAll(0),
-                                        shape: MaterialStateProperty.all<
-                                                RoundedRectangleBorder>(
-                                            RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.circular(
-                                              Dimension.height20),
+                                        elevation: const MaterialStatePropertyAll(0),
+                                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(Dimension.height20),
                                         )),
-                                        backgroundColor:
-                                            const MaterialStatePropertyAll(
-                                                AppColors.blueColor)),
+                                        backgroundColor: const MaterialStatePropertyAll(AppColors.blueColor)),
                                     onPressed: () {
                                       uploadImageDialog();
                                     },
@@ -271,8 +247,7 @@ class _EditSizeScreenState extends State<EditSizeScreen> {
                                         top: Dimension.height12,
                                         bottom: Dimension.height16),
                                     child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
+                                      crossAxisAlignment: CrossAxisAlignment.start,
                                       children: [
                                         Column(
                                           children: [
@@ -306,23 +281,15 @@ class _EditSizeScreenState extends State<EditSizeScreen> {
                           : Container(
                               height: Dimension.height56,
                               color: Colors.white,
-                              padding: EdgeInsets.symmetric(
-                                  horizontal: Dimension.width16,
-                                  vertical: Dimension.height8),
+                              padding: EdgeInsets.symmetric(horizontal: Dimension.width16, vertical: Dimension.height8),
                               child: ElevatedButton(
                                   onPressed: _hanldeEditSize,
                                   style: ButtonStyle(
-                                      elevation:
-                                          const MaterialStatePropertyAll(0),
-                                      shape: MaterialStateProperty.all<
-                                              RoundedRectangleBorder>(
-                                          RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(
-                                            Dimension.height20),
+                                      elevation: const MaterialStatePropertyAll(0),
+                                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(Dimension.height20),
                                       )),
-                                      backgroundColor:
-                                          const MaterialStatePropertyAll(
-                                              AppColors.blueColor)),
+                                      backgroundColor: const MaterialStatePropertyAll(AppColors.blueColor)),
                                   child: Text(
                                     "Edit Size",
                                     style: AppText.style.regularWhite16,
